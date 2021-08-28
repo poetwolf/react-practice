d2f310c0b40ea065053071cef72612c33657b3fa
poetwolf <2532637185@qq.com>
2021-08-28 21:19:40 +0800

    init project

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom';
+
+import EventComponent from './events/EventComponent';
+
+const App = () => {
+  return (
+    <div>
+      <EventComponent />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
